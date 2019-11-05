ba77f2306f2fcbad2a02818775e4251f3f35e474
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-11-05 20:44:28 +0100

    initial commit

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.weatherapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()){
+                    case R.id.action_weather:
+                        Toast.makeText(MainActivity.this,"Weather Clicked",Toast.LENGTH_SHORT).show();
+                    break;
+
+                    case R.id.action_settings:
+                        Toast.makeText(MainActivity.this," Settings Clicked",Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                return true;
+            }
+        });
+    }
+}
---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.example.weatherapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}

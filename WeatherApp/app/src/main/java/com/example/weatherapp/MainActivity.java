e6cd71bc4143a0f6793c097816508d61d913a360
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-11-09 12:17:21 +0100

    changed request to weather api

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -8,20 +8,25 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import java.util.List;
 
 import static androidx.appcompat.app.AppCompatDelegate.getDefaultNightMode;
 
@@ -32,9 +37,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
             setTheme(R.style.darktheme);
-        }else {
+        } else {
             setTheme(R.style.AppTheme);
         }
 
@@ -43,60 +48,45 @@ public class MainActivity extends AppCompatActivity {
 
         textViewResult = findViewById(R.id.text_view_result);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://jsonplaceholder.typicode.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        JsonPlaceholderApi jsonPlaceholderApi = retrofit.create(JsonPlaceholderApi.class);
-
-        Call<List<Post>> call = jsonPlaceholderApi.getPosts();
-
-        call.enqueue(new Callback<List<Post>>() {
-            @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                if(!response.isSuccessful()){
-                    textViewResult.setText("Code"+response.code());
-                    return;
-                }
-
-                List<Post> posts = response.body();
-
-                for(Post post : posts){
-                    String content = "";
-                    content+="ID:"+post.getId()+"\n";
-                    content+="User ID:"+post.getUserId()+"\n";
-                    content+="Title:"+post.getTitle()+"\n";
-                    content+="Text:"+post.getText()+"\n\n";
-                    textViewResult.setText(content);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
-                textViewResult.setText(t.getMessage());
-            }
-        });
+        RequestQueue queue = Volley.newRequestQueue(this);
+        jsonParse();
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch(menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.action_weather:
                         //Toast.makeText(MainActivity.this,"Weather Clicked",Toast.LENGTH_SHORT).show();
-                        Intent intentMain = new Intent(MainActivity.this,MainActivity.class);
+                        Intent intentMain = new Intent(MainActivity.this, MainActivity.class);
                         startActivity(intentMain);
                         break;
 
                     case R.id.action_settings:
                         //Toast.makeText(MainActivity.this," Settings Clicked",Toast.).show();
-                        Intent intentSettings = new Intent(MainActivity.this,SettingsActivity.class);
+                        Intent intentSettings = new Intent(MainActivity.this, SettingsActivity.class);
                         startActivity(intentSettings);
                         break;
                 }
                 return true;
             }
         });
+    }
+
+    public void jsonParse() {
+        String apiurl = "http://api.openweathermap.org/data/2.5/weather?q=Ko%C5%A1ice,sk&mode=HTML&appid=91d6e79307bb2126fb26e2d4f5e21bf7";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, apiurl, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i("Result request", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Error", error.getLocalizedMessage());
+            }
+        });
+        Volley.newRequestQueue(getBaseContext()).add(request);
     }
 }
